65021a5dddc22ce9e56e11b98a82e7562069063c
Austen Blokker <eablokker@gmail.com>
2017-10-15 21:54:36 -0700

    Ability to comb multiple selections, re-select new range lengths after comb

--- CSScomb.espressoplugin/Scripts/csscombAction.js
+++ CSScomb.espressoplugin/Scripts/csscombAction.js
@@ -18,28 +18,51 @@ action.performWithContext = function(context, outError) {
 
 	var scriptErrorMessage = 'CSScomb Node script failed';
 
-	var clearRecipe = new CETextRecipe();
+	// Comb entire file if no selection
 	var processFile = '';
 	if (selection.length === 0) {
 		var documentRange = new Range(0, context.string.length);
+
+		var clearRecipe = new CETextRecipe();
 		clearRecipe.deleteRange(documentRange);
+		// Stop script if this recipe fails
+		if (!context.applyTextRecipe(clearRecipe)) return false;
 
 		processFile = '-f ';
 	}
 
-	// Stop script if this recipe fails
-	if (!context.applyTextRecipe(clearRecipe)) return false;
+	var firstSnippet = new CETextSnippet('`if ! node -v > /dev/null; then echo "$EDITOR_SELECTION"; '+dialog(nodeMissingMessage)+'; elif ! test -d '+pluginsPath+'; then echo "$EDITOR_SELECTION"; '+dialog(scriptMissingMessage, 'caution')+'; else node '+scriptPath+' '+processFile+'-a "'+method+'" -t "'+tabString+'" -l "'+lineEndingString+'" || { '+dialog(scriptErrorMessage, 'caution')+'; echo "$EDITOR_SELECTION"; }; fi`');
 
-	var snippet = new CETextSnippet('`if ! node -v > /dev/null; then echo "$EDITOR_SELECTION"; '+dialog(nodeMissingMessage)+'; elif ! test -d '+pluginsPath+'; then echo "$EDITOR_SELECTION"; '+dialog(scriptMissingMessage, 'caution')+'; else node '+scriptPath+' '+processFile+'-a "'+method+'" -t "'+tabString+'" -l "'+lineEndingString+'" || { '+dialog(scriptErrorMessage, 'caution')+'; echo "$EDITOR_SELECTION"; }; fi`');
+	var snippet = new CETextSnippet('`node '+scriptPath+' '+processFile+'-a "'+method+'" -t "'+tabString+'" -l "'+lineEndingString+'" || { '+dialog(scriptErrorMessage, 'caution')+'; echo "$EDITOR_SELECTION"; }`');
 
 	function dialog(message, icon) {
 		return '{ nohup osascript -e \'tell application "Espresso" to display dialog "' + message + '" buttons "OK" default button 1 with title "CSScomb" with icon ' + ( icon ? icon : 'note' ) + '\' &> /dev/null& }';
 	}
-	
-	// Stop script if this snippet fails
-	if (!context.insertTextSnippet(snippet, snippetOptions)) return false;
+
+	var newSelections = [];
+	var insertedOffset = 0;
+	var insertSnippets = selections.every(function(selection, index, array) {
+		var offsetLocation = selection.location + insertedOffset;
+		context.selectedRanges = [new Range(offsetLocation, selection.length)];
+
+		if (index === 0) {
+			if (!context.insertTextSnippet(firstSnippet, snippetOptions)) return false;
+		} else {
+			if (!context.insertTextSnippet(snippet, snippetOptions)) return false;
+		}
+
+		insertedOffset = context.selectedRanges[0].location - (selection.location + selection.length);
+		newSelections[index] = new Range(offsetLocation, context.selectedRanges[0].location - offsetLocation);
+		return true;
+	});
+
+	if (!insertSnippets) return false;
 
 	// Scroll back to where cursor was before
-	context.selectedRanges = selections;
+	if (selection.length === 0) {
+		context.selectedRanges = [selection];
+	} else {
+		context.selectedRanges = newSelections;
+	}
 	return true;
 };
